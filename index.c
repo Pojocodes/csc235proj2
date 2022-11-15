c778ccd50fdbb59b7602322c1e26dc984951d820
petercode <pojo497@stu.ui.edu.ng>
2022-11-15 15:59:57 +0100

    finsh writing the program

--- index.c
+++ index.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#include <stdbool.h>
 
+//  a function to make all the case upper i.e("UUUDDD")
 const char *changeCase(char *word ){
     int length = strlen(word);
     for(int i = 0; i<length; i++){
-        word[i] = toupper(word[i]);
+        word[i] = toupper(word[i]);   // this makes it upper case
     }
     
 }
 
+// A function to check how many times adam goes up
 int countSteps(char *steps){
     int step = 0;
-    int length = strlen(steps);
-    for(int i = 0; i < length; i++){
+    int length = strlen(steps);   // find the length of the array
+    for(int i = 0; i < length; i++){  // a loop to count how many times adam goes up before he comes down
         if(steps[i] == 'U'){
             step+=1;
         }
@@ -32,22 +33,19 @@ int countSteps(char *steps){
 
 
 int main() {
-    FILE *fh_input;
-    fh_input = fopen("ada.txt","r");// open the file for writing
+    FILE *fh_input;   //create the input file
+    fh_input = fopen("adam.txt","r");// open the file for writing
     char finput[100];
-    bool x = false;
     char buf[100];
-    if(fh_input != NULL){
+    if (fh_input != NULL) {        // if the file path is correct
         fgets(buf, 100, fh_input);
         while(fscanf(fh_input, "%s", finput) != EOF){
             changeCase(finput);
             printf("%s = %d trials\n", finput,countSteps(finput));
-            x = true;
         }
     }
     
-    else {
-        
+    else {  //  else the program would collect input from the console
         int length;
         printf("put the maximum no:\n");
         scanf("%d", &length);
@@ -60,6 +58,5 @@ int main() {
             printf("%s = %d trials\n", user_input, countSteps(user_input));
         }
     }
-    
     return 0;
 }
